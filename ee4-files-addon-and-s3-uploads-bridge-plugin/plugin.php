e3d5e9c8aa5a2ac957c894adc9842a9b7e015c74
Alex Gobert <alexandergobert@utexas.edu>
2025-03-01 21:38:03 -0600

    ver1.2 - added parser to filename to insert timestamp before file extension. previously causing issues with EE4 files addon due to invalid file uri.

--- ee4-files-addon-and-s3-uploads-bridge-plugin/plugin.php
+++ ee4-files-addon-and-s3-uploads-bridge-plugin/plugin.php
@@ -6,7 +6,7 @@ Plugin URI:  https://github.com/alexgobert/ee4-files-addon-functionality
 Description: Functionality plugin to improve the EE4 files addon, last updated in 2017. 
 Author:      Alex Gobert, 2024-2025 UNAVSA Information Technology Director
 Author URI:  https://alexgobert.github.io/
-Version:     1.1
+Version:     1.2
 License:     GPL-3.0
 License URI: https://opensource.org/license/gpl-3-0
 */
@@ -21,8 +21,23 @@ function normalize_filename(string $filename) {
     $offset = $current_date->format('O');
     $offset = str_replace('+', 'p', str_replace('-', 'm', $offset));
 
-    return "$filename-$current_date_str$offset";
+    $parsed = parse_url($filename);
+    $parsed['path'] = pathinfo($parsed['path'], PATHINFO_FILENAME) . '-' . $current_date_str . $offset . '.' . pathinfo($parsed['path'], PATHINFO_EXTENSION);
+
+    return unparse_url($parsed);
 }
 
+// from https://www.php.net/manual/en/function.parse-url.php#106731
+function unparse_url($parsed_url) {
+    $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+    $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+    $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+    $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+    $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
+    $pass     = ($user || $pass) ? "$pass@" : '';
+    $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+    $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+    $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
 
-// bridge compatibility of the EE4 files addon and humanmade/s3-uploads. See https://unavsait.atlassian.net/browse/UIT-123?atlOrigin=eyJpIjoiYWIyNjhjYzI2MWRhNDZjMDg3ZTlhZTMzYWJlNGY5OWQiLCJwIjoiaiJ9
+    return "$scheme$user$pass$host$port$path$query$fragment";
+  }
